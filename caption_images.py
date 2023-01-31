2011b80d4b72b14f4fbb372c8328beed51e76553
flak <flak.yar@gmail.com>
2023-01-31 16:54:58 +0300

    up

---
+++ caption_images.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import argparse
+from .main import Connection, default_url
+
+def main(args):
+    conn = Connection(default_url)
+    images = list(Path(args.folder).glob("*.jpg"))
+    if not images:
+        images = list(Path(args.folder).glob("*.png"))
+    for image in images:
+        caption = conn.interrogate(image.as_posix())
+        (image.parent / image.stem / ".txt").write_text(caption)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("folder", default=".")
+    args = parser.parse_args()
+
+    main(args)
