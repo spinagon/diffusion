77f8dc74f79e803f98e276a24e5d03b340f5693b
flak <flak.yar@gmail.com>
2023-03-09 11:25:38 +0300

    up

--- horde.py
+++ horde.py
@@ -24,13 +24,15 @@ class Connection:
             print(r)
             print(r.text)
 
-    def generate(self, prompt, options=None):
+    def generate(self, prompt, source_image=None, options=None):
         if options == None:
             options = {}
         headers = {"apikey": self.api_key}
         params = {"sampler_name": "k_dpmpp_2m", "steps": 20}
         params.update(options)
         payload = {"prompt": prompt, "params": params, "models": ["Deliberate"]}
+        if source_image:
+            payload["source_image"] = source_image
         r = requests.post(
             self.endpoint + "/generate/async", json=payload, headers=headers
         )
@@ -49,17 +51,22 @@ class Connection:
 
     def txt2img(self, prompt, options=None):
         uuid = self.generate(prompt, options)
-        wait_list = [5, 1, 1, 1, 2, 3, 4] + [15] * 100
-        for i in range(100):
-            time.sleep(wait_list[i])
-            d = self.status(uuid, "image")
-            if "message" in d:
-                print(message)
-            if d.get("finished", 0) == 1:
-                return save(d)
+        d = self.await_result(uuid, "image")
+        return save(d)
+
+    def img2img(self, prompt, img, options=None, denoise=0.55):
+        if options is None:
+            options = {}
+        options["denoising_strength"] = denoise
+        options["sampler_name"] = "k_euler_a"
+        uuid = self.generate(prompt, source_image=pack_image(img), options=options)
+        d = self.await_result(uuid, "image")
+        return save(d)
 
     def caption(self, img):
-        uuid = self.interrogate(img, kind)
+        uuid = self.interrogate(img, "caption")
+        d = self.await_result(uuid, "interrogate")
+        return d
 
     def interrogate(self, img, kind="caption"):
         headers = {"apikey": self.api_key}
@@ -67,7 +74,17 @@ class Connection:
         r = requests.post(
             self.endpoint + "/interrogate/async", json=payload, headers=headers
         )
+        return r.json()["id"]
 
+    def await_result(self, uuid, kind):
+        wait_list = [5, 2, 1, 1, 2, 3, 4] + [15] * 100
+        for i in range(100):
+            time.sleep(wait_list[i])
+            d = self.status(uuid, kind)
+            if "message" in d:
+                print(message)
+            if d.get("finished", 0) == 1 or d.get("state", 0) == "done":
+                return d
 
 def prepare_path():
     path = "./sd/" + datetime.datetime.now().isoformat().split(".")[0].replace(":", ".")
