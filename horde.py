e2085d0f92b4fc8b216c773fff9aa00c3c313054
flak <flak.yar@gmail.com>
2023-03-10 12:09:15 +0300

    up

--- horde.py
+++ horde.py
@@ -3,6 +3,8 @@ import time
 from pathlib import Path
 import datetime
 import base64
+from io import BytesIO
+import re
 
 
 class Connection:
@@ -28,6 +30,8 @@ class Connection:
         if options == None:
             options = {}
         headers = {"apikey": self.api_key}
+        if "seed" in options:
+            options["seed"] = str(options["seed"])
         params = {"sampler_name": "k_dpmpp_2m", "steps": 20}
         params.update(options)
         payload = {"prompt": prompt, "params": params, "models": ["Deliberate"]}
@@ -50,18 +54,20 @@ class Connection:
         return r.json()
 
     def txt2img(self, prompt, options=None):
+        path = prepare_path(prompt) + ".webp"
         uuid = self.generate(prompt, options)
         d = self.await_result(uuid, "image")
-        return save(d)
+        return save(d, path)
 
     def img2img(self, prompt, img, options=None, denoise=0.55):
+        path = prepare_path(prompt) + ".webp"
         if options is None:
             options = {}
         options["denoising_strength"] = denoise
         options["sampler_name"] = "k_euler_a"
         uuid = self.generate(prompt, source_image=pack_image(img), options=options)
         d = self.await_result(uuid, "image")
-        return save(d)
+        return save(d, path)
 
     def caption(self, img):
         uuid = self.interrogate(img, "caption")
@@ -86,18 +92,34 @@ class Connection:
             if d.get("finished", 0) == 1 or d.get("state", 0) == "done":
                 return d
 
-def prepare_path():
-    path = "./sd/" + datetime.datetime.now().isoformat().split(".")[0].replace(":", ".")
+def prepare_path(prompt=""):
+    path = "./sd/" + datetime.datetime.now().isoformat().split(".")[0].replace(":", ".") + "_" + get_slug(prompt)
     Path(path).parent.mkdir(parents=True, exist_ok=True)
     return path
 
 
-def save(result):
+def save(result, path):
     data = requests.get(result["generations"][0]["img"]).content
-    path = prepare_path() + ".webp"
     Path(path).write_bytes(data)
     return path
 
 
-def pack_image(img):
-    return base64.encodebytes(Path(img).read_bytes()).decode()
+def pack_image(img, format=None):
+    if isinstance(img, str) or isinstance(img, Path):
+        image = Path(img).read_bytes()
+        if format is None:
+            format = Path(img).suffix.strip(".")
+    if isinstance(img, np.ndarray):
+        if img.dtype == np.uint8:
+            img_8 = img
+        else:
+            img_8 = (img * 255).astype(np.uint8)
+        data = BytesIO()
+        if format is None:
+            format = "png"
+        imageio.imwrite(data, img_8, format=format)
+        image = data.getvalue()
+    return base64.encodebytes(image).decode()
+
+def get_slug(prompt):
+    return '_'.join(re.findall("[a-zA-Z]+", prompt))[:40]
