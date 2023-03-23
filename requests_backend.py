a3b2ddcabdd81d60f954a1ea186ed81f2bea9210
flak <flak.yar@gmail.com>
2023-03-23 10:25:22 +0300

    up

--- requests_backend.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-class Http_backend:
-    def get(*args, **kwargs):
-        return requests.get(*args, **kwargs)
-
-    def post(*args, **kwargs):
-        return requests.post(*args, **kwargs)
