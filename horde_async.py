c23a6efa1d8338e94f23e7fe8be80f789af7d74b
spinagon <spinagon@users.noreply.github.com>
2023-06-09 06:54:29 +0000

    Format code with black

--- horde_async.py
+++ horde_async.py
@@ -248,7 +248,9 @@ class Job:
     async def generate(self):
         for i in range(3):
             r = await requests.post(
-                self.conn.endpoint + "/generate/async", json=self.payload, headers=self.headers
+                self.conn.endpoint + "/generate/async",
+                json=self.payload,
+                headers=self.headers,
             )
             if r.status_code != 403:
                 break
@@ -267,9 +269,15 @@ class Job:
 
     async def status(self):
         if self.kind == "interrogate":
-            r = await requests.get(self.conn.endpoint + "/interrogate/status/" + self.uuid, headers=self.headers)
+            r = await requests.get(
+                self.conn.endpoint + "/interrogate/status/" + self.uuid,
+                headers=self.headers,
+            )
         else:
-            r = await requests.get(self.conn.endpoint + "/generate/status/" + self.uuid, headers=self.headers)
+            r = await requests.get(
+                self.conn.endpoint + "/generate/status/" + self.uuid,
+                headers=self.headers,
+            )
         try:
             status = r.json()
             if "prompt" in status:
@@ -353,8 +361,10 @@ class Interrogation_job(Job):
     async def interrogate(self):
         for i in range(3):
             r = await requests.post(
-                self.conn.endpoint + "/interrogate/async", json=self.payload, headers=self.headers
-)
+                self.conn.endpoint + "/interrogate/async",
+                json=self.payload,
+                headers=self.headers,
+            )
             if r.status_code != 403:
                 break
             else:
