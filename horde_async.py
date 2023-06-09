2b701fa5c0cff5056e84e09bef4c61f1f9f5b7a0
spinagon <spinagon@users.noreply.github.com>
2023-06-09 06:50:24 +0000

    Format code with black

--- horde_async.py
+++ horde_async.py
@@ -248,7 +248,9 @@ class Job:
     async def generate(self):
         for i in range(3):
             r = await requests.post(
-                self.endpoint + "/generate/async", json=self.payload, headers=self.headers
+                self.endpoint + "/generate/async",
+                json=self.payload,
+                headers=self.headers,
             )
             if r.status_code != 403:
                 break
@@ -267,9 +269,13 @@ class Job:
 
     async def status(self):
         if self.kind == "interrogate":
-            r = await requests.get(self.endpoint + "/interrogate/status/" + self.uuid, headers=self.headers)
+            r = await requests.get(
+                self.endpoint + "/interrogate/status/" + self.uuid, headers=self.headers
+            )
         else:
-            r = await requests.get(self.endpoint + "/generate/status/" + self.uuid, headers=self.headers)
+            r = await requests.get(
+                self.endpoint + "/generate/status/" + self.uuid, headers=self.headers
+            )
         try:
             status = r.json()
             if "prompt" in status:
@@ -353,7 +359,9 @@ class Interrogation_job(Job):
     async def interrogate(self):
         for i in range(3):
             r = await requests.post(
-                self.endpoint + "/interrogate/async", json=self.payload, headers=self.headers
+                self.endpoint + "/interrogate/async",
+                json=self.payload,
+                headers=self.headers,
             )
             if r.status_code != 403:
                 break
