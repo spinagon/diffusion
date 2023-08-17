c04ccdd4fe5be469da4c892f76b75887f709244c
spinagon <spinagon@users.noreply.github.com>
2023-08-17 13:48:49 +0000

    Format code with black

--- comfy.py
+++ comfy.py
@@ -8,7 +8,7 @@ prompt_text = "cat photo, nature, lakeside"
 
 def queue_prompt(prompt):
     p = {"prompt": prompt}
-    data = json.dumps(p).encode('utf-8')
+    data = json.dumps(p).encode("utf-8")
     r = requests.post("http://127.0.0.1:8188/prompt", json=p)
     print(r.text)
     r = requests.get("http://127.0.0.1:8188/queue")
@@ -16,10 +16,10 @@ def queue_prompt(prompt):
 
 
 prompt = json.loads(Path("c:/prog/1/comfy_1.json").read_text())
-#set the text prompt for our positive CLIPTextEncode
+# set the text prompt for our positive CLIPTextEncode
 prompt["4"]["inputs"]["text"] = prompt_text
 
-#set the seed for our KSampler node
-prompt["1"]["inputs"]["seed"] = 505193631#random.randint(0, int(1e10))
+# set the seed for our KSampler node
+prompt["1"]["inputs"]["seed"] = 505193631  # random.randint(0, int(1e10))
 
 queue_prompt(prompt)
